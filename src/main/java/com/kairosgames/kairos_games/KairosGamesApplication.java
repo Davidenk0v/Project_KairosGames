a5e10fafa2ead9bf3076bf53560175a74f0ce89c
David <davidalvper@gmail.com>
2024-03-02 12:46:26 +0000

    spring initializr

---
+++ src/main/java/com/kairosgames/kairos_games/KairosGamesApplication.java
@@ -0,0 +1,13 @@
+package com.kairosgames.kairos_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairosGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KairosGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kairosgames/kairos_games/KairosGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairosgames.kairos_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
